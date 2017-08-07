6c418801995a0ee2477c5fecd4ddadb4d9e6fd47
ksrangsy <ken.rangsy@live.com>
2017-08-06 23:02:17 -0500

    Initial Tasks service.

---
+++ GoogleLikeWorks/Controllers/TasksController.cs
@@ -0,0 +1,58 @@
+﻿using GoogleLikeWorks.Models;
+using GoogleLikeWorks.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GoogleLikeWorks.Controllers
+{
+    public class TasksController : ApiController
+    {
+        public IEnumerable<TasksModel> Get()
+        {
+            var tasks = TasksRepository.GetAll();
+
+            return tasks;
+        }
+
+        public TasksModel Get(int taskID)
+        {
+            var page = TasksRepository.Get(taskID);
+
+            return page;
+        }
+
+        public int Post([FromBody]TasksModel task)
+        {
+            var id = TasksRepository.NewTask(task.PageID, task.Blob);
+
+            return id;
+        }
+
+        public int Put(int id, [FromBody]TasksModel task)
+        {
+            var result = TasksRepository.Get(id);
+
+            if (result != null)
+            {
+                var resultId = TasksRepository.NewTask(task.PageID, task.Blob);
+
+                return resultId;
+            }
+            else
+            {
+                TasksRepository.UpdateTask(task.ID, task.Blob);
+            }
+
+            return 0;
+        }
+
+        public void Delete(int id)
+        {
+            TasksRepository.DeleteTask(id);
+        }
+    }
+}
---
+++ GoogleLikeWorks/Repositories/TasksRepository.cs
@@ -0,0 +1,67 @@
+﻿using Dapper;
+using GoogleLikeWorks.Models;
+using GoogleLikeWorks.Repositories.Helpers;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace GoogleLikeWorks.Repositories
+{
+    internal class TasksRepository
+    {
+        internal static List<TasksModel> GetAll()
+        {
+            using (IDbConnection db = Database.DbConnection())
+            {
+                var tasks = db.Query<TasksModel>("SELECT * FROM Tasks").ToList();
+
+                return tasks;
+            }
+        }
+
+        internal static TasksModel Get(int taskID)
+        {
+            using (IDbConnection db = Database.DbConnection())
+            {
+                var sql = @"SELECT * FROM Tasks WHERE id = @id;";
+
+                var task = db.Query<TasksModel>(sql, new { id = taskID }).FirstOrDefault();
+
+                return task;
+            }
+        }
+
+        internal static int NewTask(int pageID, string blob)
+        {
+            using (IDbConnection db = Database.DbConnection())
+            {
+                var sql = @"INSERT INTO Tasks(PageID, Blob) VALUES(@id, @blob);
+                    SELECT CAST(SCOPE_IDENTITY() AS INT);";
+
+                var id = db.Query<int>(sql, new { id = pageID, blob = blob }).SingleOrDefault();
+
+                return id;
+            }
+        }
+
+        internal static void DeleteTask(int taskID)
+        {
+            using (IDbConnection db = Database.DbConnection())
+            {
+                var sql = @"DELETE Tasks WHERE ID = @id;";
+
+                var results = db.Execute(sql, new { id = taskID });
+            }
+        }
+
+        internal static void UpdateTask(int taskID, string blob)
+        {
+            using (IDbConnection db = Database.DbConnection())
+            {
+                var sql = @"UPDATE Tasks SET Blob = @blob WHERE ID = @id;";
+
+                var results = db.Execute(sql, new { id = taskID, blob = blob });
+            }
+        }
+    }
+}
